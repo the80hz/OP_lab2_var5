8ef43686d190c0d9b4bbc4036d9ad760fbc8d98a
the80hz <dnl.vlgv@mail.ru>
2022-12-22 23:35:30 +0400

    test2

--- main.c
+++ main.c
@@ -21,31 +21,44 @@ double factorial(int n) {
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int n = 10, x = 0;
-    double eps = 0;
-    double cosx, sinx, tgx, ctgx;
-    double sum = 0;
-
     printf("Select an action:\n1. The number of expansion terms N > 10\n 2. The accuracy of eps calculations > 0.\n");
     int choice;
     scanf("%d", &choice);
 
+    double sum, cosx, sinx, tgx, ctgx;
+
     switch (choice) {
         case 1:
-            while(n <= 10) {
+            printf("Enter the number of expansion members N > 10: ");
+            int N;
+            scanf("%d", &N);
+            while(N <= 10) {
                 printf("Enter the number of expansion members N > 10: ");
-                scanf("%d", &n);
+                scanf("%d", &N);
             }
             printf("Enter the angle x in radians: ");
+            int x;
             scanf("%d", &x);
-            for (int i = 0; i <= n; i++) {
+            for (int i = 0; i <= N; i++) {
                 sum += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
 
                 printf("Step %d: cos(%d) = %f\n", i, x, sum);
             }
+
+            cosx = sum;
+            sinx = sqrt(1 - pow(cosx, 2));
+            tgx = sinx / cosx;
+            ctgx = cosx / sinx;
+            printf("cos(%d) = %f\n", x, cosx);
+            printf("sin(%d) = %f\n", x, sinx);
+            printf("tg(%d) = %f\n", x, tgx);
+            printf("ctg(%d) = %f\n", x, ctgx);
             break;
 
         case 2:
+            printf("Enter the calculation accuracy eps > 0: ");
+            double eps = 0;
+            scanf("%lf", &eps);
             while(eps <= 0) {
                 printf("Enter the calculation accuracy eps > 0: ");
                 scanf("%lf", &eps);
@@ -58,20 +71,22 @@ int main()
 
                 printf("Step %d: cos(%d) = %f\n", i, x, sum);
             }
+
+            cosx = sum;
+            sinx = sqrt(1 - pow(cosx, 2));
+            tgx = sinx / cosx;
+            ctgx = cosx / sinx;
+
+            printf("cos(%d) = %f\n", x, cosx);
+            printf("sin(%d) = %f\n", x, sinx);
+            printf("tg(%d) = %f\n", x, tgx);
+            printf("ctg(%d) = %f\n", x, ctgx);
         default:
             printf("Invalid input.\n");
             break;
     }
 
-    cosx = sum;
-    sinx = sqrt(1 - pow(cosx, 2));
-    tgx = sinx / cosx;
-    ctgx = cosx / sinx;
 
-    printf("cos(%d) = %f\n", x, cosx);
-    printf("sin(%d) = %f\n", x, sinx);
-    printf("tg(%d) = %f\n", x, tgx);
-    printf("ctg(%d) = %f\n", x, ctgx);
 
     return 0;
 }
