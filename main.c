013e4476cd93aabb96399b607a2efcba210bae9c
the80hz <dnl.vlgv@mail.ru>
2023-01-09 19:15:07 +0400

    fin

--- main.c
+++ main.c
@@ -9,16 +9,37 @@ double factorial(int n) {
     return f;
 }
 
+double func(int i, int x) {
+    double a = pow(-1, i);
+    double b = pow(x, 2 * i + 1);
+    double c = factorial(2 * i + 1);
+    return a * b / c;
+}
+
+double fsinx(double cosx) {
+    return sqrt(1 - cosx * cosx);
+}
+
+double ftgx(double sinx, double cosx) {
+    return sinx / cosx;
+}
+
+double fctgx(double sinx, double cosx) {
+    return cosx / sinx;
+}
+
 int main()
 {
     printf("Select an action:\n1. The number of expansion terms N > 10\n 2. The accuracy of eps calculations > 0.\n");
     int choice;
     scanf("%d", &choice);
 
-    double sum, cosx, sinx, tgx, ctgx;
+    double sum = 0, cosx, sinx, tgx, ctgx;
+    int x = 0;
 
     switch (choice) {
         case 1:
+        {
             printf("Enter the number of expansion members N > 10: ");
             int N;
             scanf("%d", &N);
@@ -27,23 +48,16 @@ int main()
                 scanf("%d", &N);
             }
             printf("Enter the angle x in radians: ");
-            int x;
+
             scanf("%d", &x);
             for (int i = 0; i <= N; i++) {
                 sum += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
 
                 printf("Step %d: cos(%d) = %f\n", i, x, sum);
             }
-
-            cosx = sum;
-            sinx = sqrt(1 - pow(cosx, 2));
-            tgx = sinx / cosx;
-            ctgx = cosx / sinx;
-            printf("cos(%d) = %f\n", x, cosx);
-            printf("sin(%d) = %f\n", x, sinx);
-            printf("tg(%d) = %f\n", x, tgx);
-            printf("ctg(%d) = %f\n", x, ctgx);
             break;
+        }
+
 
         case 2:
             printf("Enter the calculation accuracy eps > 0: ");
@@ -56,26 +70,25 @@ int main()
             printf("Enter the angle x in radians: ");
             scanf("%d", &x);
 
-            for (int i = 0; fabs(pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1)) > eps; i++) {
-                sum += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
+            for (int i = 0; fabs(func(i, x)) > eps; i++) {
+                sum += func(i, x);
 
                 printf("Step %d: cos(%d) = %f\n", i, x, sum);
             }
 
-            cosx = sum;
-            sinx = sqrt(1 - pow(cosx, 2));
-            tgx = sinx / cosx;
-            ctgx = cosx / sinx;
-
-            printf("cos(%d) = %f\n", x, cosx);
-            printf("sin(%d) = %f\n", x, sinx);
-            printf("tg(%d) = %f\n", x, tgx);
-            printf("ctg(%d) = %f\n", x, ctgx);
         default:
-            printf("Invalid input.\n");
             break;
     }
 
+    cosx = sum;
+    sinx = fsinx(cosx);
+    tgx = ftgx(sinx, cosx);
+    ctgx = fctgx(sinx, cosx);
+
+    printf("cos(%d) = %f\n", x, cosx);
+    printf("sin(%d) = %f\n", x, sinx);
+    printf("tg(%d) = %f\n", x, tgx);
+    printf("ctg(%d) = %f\n", x, ctgx);
 
 
     return 0;
